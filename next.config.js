a487176b76a88625fd73e1ed659d79aa7a0d53c6
Yeghishe Hovhannisyan <yeghishex.hovhannisyan@winfinity.live>
2023-04-19 14:07:08 +0400

    project is configured

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.plugins.push(new MiniCssExtractPlugin());
+    config.module.rules.push({
+      test: /\.(pcss)$/,
+      use: [
+          { loader: "style-loader" },
+          { loader: "css-modules-typescript-loader" },
+          {
+              loader: "css-loader",
+              options: {
+                  modules: true,
+                  sourceMap: true,
+                  importLoaders: 1
+              }
+          },
+          { loader: "postcss-loader", options: { sourceMap: true } }
+      ]
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig
---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "postcss-nested": {},
+    "autoprefixer": {},
+    "postcss-custom-media": {},
+    'postcss-simple-vars': {},
+    "postcss-preset-env": {
+      browsers: "last 2 versions"
+    }
+  },
+}
